d7f0c2622dee4441119830d9c4096942255aebdf
ergouwang <330035385@qq.com>
2020-08-13 23:35:45 +0800

    edit 组件布局完成

--- src/components/edit.vue
+++ src/components/edit.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        edit
+    <div class="edit">
+        <div class="edit-list">
+            <button>运行</button>
+            <button>取消</button>
+        </div>
+        <div class="edit-box">
+            <textarea></textarea>
+        </div>
     </div>
 </template>
 
@@ -10,6 +16,29 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .edit{
+        .edit-list{
+            padding: 10px;
+            background-color: #ccc;
+            button{
+                width: 50px;
+                height: 30px;
+            }
+        }
+        .edit-box{
+            position: absolute;
+            top: 50px;
+            bottom: 0;
+            left: 0;
+            right: 50%;
+            textarea{
+                border: none;
+                width: 100%;
+                height: 100%;
+                outline: none;
+            }
+        }
+    }
 
 </style>
